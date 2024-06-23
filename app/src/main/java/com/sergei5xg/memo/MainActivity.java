87f337563cf2f9847859d9027b1288095b447a6f
Stan5xg <sudostan@gmail.com>
2024-06-23 16:41:13 +0300

    Initial commit

---
+++ app/src/main/java/com/sergei5xg/memo/MainActivity.java
@@ -0,0 +1,106 @@
+package com.sergei5xg.memo;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    private String jsonString = "[{\"question\":\"Eğitimi\",\"answer\":\"training\"}, {\"question\":\"Kitap\",\"answer\":\"book\"}]";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout container = findViewById(R.id.question_container);
+
+        try {
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String question = jsonObject.getString("question");
+                String answer = jsonObject.getString("answer");
+
+                LinearLayout questionBlock = new LinearLayout(this); // Horizontal layout
+                questionBlock.setOrientation(LinearLayout.HORIZONTAL);
+
+                TextView questionTextView = new TextView(this);
+                questionTextView.setText(question);
+
+                Button checkButton = new Button(this);
+                checkButton.setText("check");
+
+                checkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, answer, Toast.LENGTH_SHORT).show(); // Display answer in Toast
+                    }
+                });
+
+                questionBlock.addView(questionTextView);
+                questionBlock.addView(checkButton);
+                container.addView(questionBlock);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
